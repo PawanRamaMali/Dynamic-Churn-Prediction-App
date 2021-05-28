5cee9b21b84fef30b8ba9985e777ea8f98b2149f
Pawan Rama Mali <prm@outlook.in>
2021-05-28 23:01:36 +0530

    v0.00

---
+++ app/Login Module/global.R
@@ -0,0 +1,50 @@
+# global.R
+
+rm(list=ls())
+
+# LIBRARIES ----
+
+# Shiny
+library(shiny)
+library(bslib)
+
+# Modeling
+#library(modeldata)
+library(DataExplorer)
+
+# Widgets
+library(plotly)
+
+# Core
+library(tidyverse)
+#if (!require("DT")) install.packages('DT')
+library(DT)
+
+library(ggplot2)
+
+library(rmarkdown)
+library(knitr)
+library(pander)
+
+library(shiny)
+library(shinydashboard)
+library(shinyAce)
+library(shinyWidgets)
+library(data.table)
+library(esquisse)
+library(rvg)
+library(magrittr)
+
+
+## Setting Upload Size to 1GB
+options(shiny.maxRequestSize = 1000000000)
+options("esquisse.display.mode" = "browser")
+
+# LOAD DATASETS ----
+# utils::data("stackoverflow", "car_prices", "Sacramento", package = "modeldata")
+# 
+# data_list = list(
+#   "StackOverflow" = stackoverflow,
+#   "Car Prices"    = car_prices,
+#   "Sacramento Housing" = Sacramento
+# )
---
+++ app/Login Module/ui.R
@@ -0,0 +1,21 @@
+###################
+# ui.R
+# 
+# Initializes the ui. 
+# Used to load in your header, sidebar, and body components.
+###################
+
+# If shinydashboard library is not imported here, the dashboard does not work 
+# on Shiny Server.
+
+library(shinydashboard)
+
+source('./components/header.R')
+source('./components/sidebar.R')
+source('./components/body.R')
+
+
+ui <- dashboardPage(
+  header = header,
+  sidebar =  sidebar,
+  body = body)
