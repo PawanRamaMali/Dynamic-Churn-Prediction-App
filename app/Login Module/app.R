2ab1cd2a7c52a9ea7a01c9fed2b0dcd707e08d94
Pawan Rama Mali <prm@outlook.in>
2021-06-03 20:47:28 +0530

    ver 1.2.1

--- app/Login Module/app.R
+++ app/Login Module/app.R
@@ -1,5 +1,10 @@
 # Main controller ----
-
+# require(shiny,quietly = TRUE, warn.conflicts = FALSE)
+# require(shinyjs,quietly = TRUE, warn.conflicts = FALSE)
+# .libPaths("./R-Portable/App/R-Portable/library")
+# browser.path = file.path(getwd(),"GoogleChromePortable/GoogleChromePortable.exe")
+# options(browser = browser.path)
+# shiny::runApp("./Shiny/",port=8888,launch.browser= TRUE,quiet=TRUE)
 
 # * Load libraries ----
 source('global.R')
