541ab4ea3f5578be8074ddbcb5937dabb6af7fcb
Pawan Rama Mali <prm@outlook.in>
2021-06-04 23:30:09 +0530

    Update model_script.R

--- app/model_script.R
+++ app/model_script.R
@@ -11,70 +11,77 @@ data$Dependents_1 <- ifelse (data$Dependents == "Yes", 1, 0)
 data$PhoneService_1 <- ifelse(data$PhoneService == "Yes", 1, 0)
 
 data$MultipleLines_1 <-
-  
-  revalue(data$MultipleLines,
+  plyr::revalue(data$MultipleLines,
           c(
             "Yes" = 1,
             "No" = 0,
             "No phone service" = 2
           ))
-data$InternetService_1 <- revalue(data$InternetService, c(
-  "DSL" = 1,
-  "Fiber optic" = 2,
-  "No" = 0
+data$InternetService_1 <- 
+  plyr::revalue(data$InternetService, c(
+          "DSL" = 1,
+          "Fiber optic" = 2,
+          "No" = 0
 ))
 data$OnlineSecurity_1 <-
-  revalue(data$OnlineSecurity,
+  plyr::revalue(data$OnlineSecurity,
           c(
             "Yes" = 1,
             "No" = 0,
             "No internet service" = 2
           ))
 data$OnlineBackup_1 <-
-  revalue(data$OnlineBackup,
+  plyr::revalue(data$OnlineBackup,
           c(
             "Yes" = 1,
             "No" = 0,
             "No internet service" = 2
           ))
 data$DeviceProtection_1 <-
-  revalue(data$DeviceProtection,
+  plyr::revalue(data$DeviceProtection,
           c(
             "Yes" = 1,
             "No" = 0,
             "No internet service" = 2
           ))
 data$TechSupport_1 <-
-  revalue(data$TechSupport,
+  plyr::revalue(data$TechSupport,
           c(
             "Yes" = 1,
             "No" = 0,
             "No internet service" = 2
           ))
 data$StreamingTV_1 <-
-  revalue(data$StreamingTV,
+  plyr::revalue(data$StreamingTV,
           c(
             "Yes" = 1,
             "No" = 0,
             "No internet service" = 2
           ))
 data$StreamingMovies_1 <-
-  revalue(data$StreamingMovies,
+  plyr::revalue(data$StreamingMovies,
           c(
             "Yes" = 1,
             "No" = 0,
             "No internet service" = 2
           ))
 data$PaperlessBilling_1 <-
-  revalue(data$PaperlessBilling, c("Yes" = 1, "No" = 0))
-data$Churn_1 <- revalue(data$Churn, c("Yes" = 1, "No" = 0))
+  plyr::revalue(data$PaperlessBilling,
+                c("Yes" = 1, "No" = 0))
+
+data$Churn_1 <- plyr::revalue(
+  data$Churn, c("Yes" = 1, "No" = 0))
+
 final_data <-
   data[-c(1, 2, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 21)]
 
 set.seed(1000000)
-pred <- data.frame(predict(rf,
-                           data = final_data ,
-                           type = "prob"))
+
+pred <- data.frame(
+  stats::predict(rf,
+          data = final_data ,
+          type = "prob"))
+
 Prediction <- ifelse(pred[, 2] < 0.26, "Not_Churn", "Churn")
 probability <- round(pred[, 2], 3)
 final_tab <- data.frame(data[1], Prediction)
