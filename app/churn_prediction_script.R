dbef481f0affe012665e9a41dfac6db6b18e7801
Pawan Rama Mali <prm@outlook.in>
2021-06-05 23:00:23 +0530

    Create churn_prediction_script.R

---
+++ app/churn_prediction_script.R
@@ -0,0 +1,194 @@
+#Data Preprocessing
+library(plyr)
+library(corrplot)
+library(ggplot2)
+library(gridExtra)
+library(ggthemes)
+library(caret)
+library(MASS)
+library(randomForest)
+library(party)
+churn <- read.csv('Telco-Customer-Churn.csv')
+str(churn)
+
+#missing values
+sapply(churn, function(x) sum(is.na(x)))
+#remove 11 missing records
+churn <- churn[complete.cases(churn), ]
+
+#Recode columns
+cols_recode1 <- c(10:15)
+for(i in 1:ncol(churn[,cols_recode1])) {
+  churn[,cols_recode1][,i] <- as.factor(mapvalues
+                                        (churn[,cols_recode1][,i], from =c("No internet service"),to=c("No")))
+}
+
+churn$MultipleLines <- as.factor(
+  mapvalues(churn$MultipleLines, 
+  from=c("No phone service"),
+       to=c("No")))
+
+min(churn$tenure); max(churn$tenure)
+
+group_tenure <- function(tenure){
+  if (tenure >= 0 & tenure <= 12){
+    return('0-12 Month')
+  }else if(tenure > 12 & tenure <= 24){
+    return('12-24 Month')
+  }else if (tenure > 24 & tenure <= 48){
+    return('24-48 Month')
+  }else if (tenure > 48 & tenure <=60){
+    return('48-60 Month')
+  }else if (tenure > 60){
+    return('> 60 Month')
+  }
+}
+churn$tenure_group <- sapply(churn$tenure,group_tenure)
+churn$tenure_group <- as.factor(churn$tenure_group)
+
+churn$SeniorCitizen <- as.factor(mapvalues(churn$SeniorCitizen,
+                                           from=c("0","1"),
+                                           to=c("No", "Yes")))
+
+#Remove columns we don't need
+churn$customerID <- NULL
+churn$tenure <- NULL
+
+churn$Churn <- as.factor(mapvalues(churn$Churn,
+                                   from=c("No", "Yes"),
+                                   to=c("0","1")))
+
+#Exploratory data analysis and feature selection
+#Correlation between numerical values
+
+numeric.var <- sapply(churn, is.numeric)
+corr.matrix <- cor(churn[,numeric.var])
+corrplot(corr.matrix, main="\n\nCorrelation Plot for Numerical Variables", method="number")
+
+#They are correlated so we will remove one, Total Charges
+churn$TotalCharges <- NULL
+
+#Bar plots of categorical variables
+p1 <- ggplot(churn, aes(x=gender)) + ggtitle("Gender") + xlab("Gender") +
+  geom_bar(aes(y = 100*(..count..)/sum(..count..)), width = 0.5) + ylab("Percentage") + coord_flip() + theme_minimal()
+p2 <- ggplot(churn, aes(x=SeniorCitizen)) + ggtitle("Senior Citizen") + xlab("Senior Citizen") + 
+  geom_bar(aes(y = 100*(..count..)/sum(..count..)), width = 0.5) + ylab("Percentage") + coord_flip() + theme_minimal()
+p3 <- ggplot(churn, aes(x=Partner)) + ggtitle("Partner") + xlab("Partner") + 
+  geom_bar(aes(y = 100*(..count..)/sum(..count..)), width = 0.5) + ylab("Percentage") + coord_flip() + theme_minimal()
+p4 <- ggplot(churn, aes(x=Dependents)) + ggtitle("Dependents") + xlab("Dependents") +
+  geom_bar(aes(y = 100*(..count..)/sum(..count..)), width = 0.5) + ylab("Percentage") + coord_flip() + theme_minimal()
+grid.arrange(p1, p2, p3, p4, ncol=2)
+
+p5 <- ggplot(churn, aes(x=PhoneService)) + ggtitle("Phone Service") + xlab("Phone Service") +
+  geom_bar(aes(y = 100*(..count..)/sum(..count..)), width = 0.5) + ylab("Percentage") + coord_flip() + theme_minimal()
+p6 <- ggplot(churn, aes(x=MultipleLines)) + ggtitle("Multiple Lines") + xlab("Multiple Lines") + 
+  geom_bar(aes(y = 100*(..count..)/sum(..count..)), width = 0.5) + ylab("Percentage") + coord_flip() + theme_minimal()
+p7 <- ggplot(churn, aes(x=InternetService)) + ggtitle("Internet Service") + xlab("Internet Service") + 
+  geom_bar(aes(y = 100*(..count..)/sum(..count..)), width = 0.5) + ylab("Percentage") + coord_flip() + theme_minimal()
+p8 <- ggplot(churn, aes(x=OnlineSecurity)) + ggtitle("Online Security") + xlab("Online Security") +
+  geom_bar(aes(y = 100*(..count..)/sum(..count..)), width = 0.5) + ylab("Percentage") + coord_flip() + theme_minimal()
+grid.arrange(p5, p6, p7, p8, ncol=2)
+
+p9 <- ggplot(churn, aes(x=OnlineBackup)) + ggtitle("Online Backup") + xlab("Online Backup") +
+  geom_bar(aes(y = 100*(..count..)/sum(..count..)), width = 0.5) + ylab("Percentage") + coord_flip() + theme_minimal()
+p10 <- ggplot(churn, aes(x=DeviceProtection)) + ggtitle("Device Protection") + xlab("Device Protection") + 
+  geom_bar(aes(y = 100*(..count..)/sum(..count..)), width = 0.5) + ylab("Percentage") + coord_flip() + theme_minimal()
+p11 <- ggplot(churn, aes(x=TechSupport)) + ggtitle("Tech Support") + xlab("Tech Support") + 
+  geom_bar(aes(y = 100*(..count..)/sum(..count..)), width = 0.5) + ylab("Percentage") + coord_flip() + theme_minimal()
+p12 <- ggplot(churn, aes(x=StreamingTV)) + ggtitle("Streaming TV") + xlab("Streaming TV") +
+  geom_bar(aes(y = 100*(..count..)/sum(..count..)), width = 0.5) + ylab("Percentage") + coord_flip() + theme_minimal()
+grid.arrange(p9, p10, p11, p12, ncol=2)
+
+p13 <- ggplot(churn, aes(x=StreamingMovies)) + ggtitle("Streaming Movies") + xlab("Streaming Movies") +
+  geom_bar(aes(y = 100*(..count..)/sum(..count..)), width = 0.5) + ylab("Percentage") + coord_flip() + theme_minimal()
+p14 <- ggplot(churn, aes(x=Contract)) + ggtitle("Contract") + xlab("Contract") + 
+  geom_bar(aes(y = 100*(..count..)/sum(..count..)), width = 0.5) + ylab("Percentage") + coord_flip() + theme_minimal()
+p15 <- ggplot(churn, aes(x=PaperlessBilling)) + ggtitle("Paperless Billing") + xlab("Paperless Billing") + 
+  geom_bar(aes(y = 100*(..count..)/sum(..count..)), width = 0.5) + ylab("Percentage") + coord_flip() + theme_minimal()
+p16 <- ggplot(churn, aes(x=PaymentMethod)) + ggtitle("Payment Method") + xlab("Payment Method") +
+  geom_bar(aes(y = 100*(..count..)/sum(..count..)), width = 0.5) + ylab("Percentage") + coord_flip() + theme_minimal()
+p17 <- ggplot(churn, aes(x=tenure_group)) + ggtitle("Tenure Group") + xlab("Tenure Group") +
+  geom_bar(aes(y = 100*(..count..)/sum(..count..)), width = 0.5) + ylab("Percentage") + coord_flip() + theme_minimal()
+grid.arrange(p13, p14, p15, p16, p17, ncol=2)
+
+#Logistic Regression
+#First, we split the data into training and testing sets:
+intrain<- createDataPartition(churn$Churn,p=0.7,list=FALSE)
+set.seed(2021)
+training<- churn[intrain,]
+testing<- churn[-intrain,]
+
+#Fitting the Logistic Regression Model:
+LogModel <- glm(Churn ~ .,family=binomial(link="logit"),data=training)
+print(summary(LogModel))
+
+#Feature Analysis
+anova(LogModel, test="Chisq")
+
+#Assessing the predictive ability of the Logistic Regression model
+#testing$Churn <- as.character(testing$Churn)
+#testing$Churn[testing$Churn=="No"] <- "0"
+#testing$Churn[testing$Churn=="Yes"] <- "1"
+fitted.results <- predict(LogModel,newdata=testing,type='response')
+fitted.results <- ifelse(fitted.results > 0.5,1,0)
+misClasificError <- mean(fitted.results != testing$Churn)
+print(paste('Logistic Regression Accuracy',1-misClasificError))
+
+print("Confusion Matrix for Logistic Regression"); 
+table(testing$Churn, fitted.results > 0.5)
+
+#Odds Ratio
+library(MASS)
+exp(cbind(OR=coef(LogModel), confint(LogModel)))
+
+#Decision Tree
+#Decision Tree visualization
+
+#Convert variables used in model to factors
+training$Contract <- factor(training$Contract)
+training$tenure_group <- factor(training$tenure_group)
+training$PaperlessBilling <- factor(training$PaperlessBilling)
+
+tree <- ctree(Churn~Contract+tenure_group+PaperlessBilling, training)
+plot(tree)
+
+#Decision Tree Confusion Matrix
+testing$Contract <- factor(testing$Contract)
+testing$tenure_group <- factor(testing$tenure_group)
+testing$PaperlessBilling <- factor(testing$PaperlessBilling)
+
+pred_tree <- predict(tree, testing)
+print("Confusion Matrix for Decision Tree"); table(Predicted = pred_tree, Actual = testing$Churn)
+
+#Decision Tree Accuracy
+p1 <- predict(tree, training)
+tab1 <- table(Predicted = p1, Actual = training$Churn)
+tab2 <- table(Predicted = pred_tree, Actual = testing$Churn)
+print(paste('Decision Tree Accuracy',sum(diag(tab2))/sum(tab2)))
+
+#Random Forest
+#Initial Model
+rfModel <- randomForest(Churn ~., data = training)
+print(rfModel)
+
+#Random Forest Prediction and Confusion Matrix
+pred_rf <- predict(rfModel, testing)
+caret::confusionMatrix(pred_rf, testing$Churn)
+
+#Random Forest Error Rate
+plot(rfModel)
+
+#Tune Random Forest Model
+t <- tuneRF(training[, -18], training[, 18], stepFactor = 0.5, plot = TRUE, ntreeTry = 200, trace = TRUE, improve = 0.05)
+
+#Fit the Random Forest Model After Tuning
+rfModel_new <- randomForest(Churn ~., data = training, ntree = 200, mtry = 2, importance = TRUE, proximity = TRUE)
+print(rfModel_new)
+
+#Random Forest Predictions and Confusion Matrix After Tuning
+pred_rf_new <- predict(rfModel_new, testing)
+caret::confusionMatrix(pred_rf_new, testing$Churn)
+
+#Random Forest Feature Importance
+varImpPlot(rfModel_new, sort=T, n.var = 10, main = 'Top 10 Feature Importance')
+
