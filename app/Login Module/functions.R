36329a6be589df9fd50314f5612183a3f6db530c
Pawan Rama Mali <prm@outlook.in>
2021-07-07 23:05:23 +0530

    added process flow

--- app/Login Module/functions.R
+++ app/Login Module/functions.R
@@ -22,3 +22,90 @@ good_time <- function() {
   }
 }
 
+
+process_data <- function(data) {
+  
+  if (is.null(data)){
+    return(NULL)
+  }
+  
+  print("Processing data. . . ")
+  
+  data$Gender_1 <- ifelse(data$gender == "Male", 1, 0)
+  
+  data$partner_1 <- ifelse(data$Partner == "Yes", 1, 0)
+  
+  data$Dependents_1 <- ifelse (data$Dependents == "Yes", 1, 0)
+  
+  data$PhoneService_1 <- ifelse(data$PhoneService == "Yes", 1, 0)
+  
+  data$MultipleLines_1 <-
+    
+    revalue(data$MultipleLines,
+            c(
+              "Yes" = 1,
+              "No" = 0,
+              "No phone service" = 2
+            ))
+  data$InternetService_1 <- revalue(data$InternetService, c(
+    "DSL" = 1,
+    "Fiber optic" = 2,
+    "No" = 0
+  ))
+  data$OnlineSecurity_1 <-
+    revalue(data$OnlineSecurity,
+            c(
+              "Yes" = 1,
+              "No" = 0,
+              "No internet service" = 2
+            ))
+  data$OnlineBackup_1 <-
+    revalue(data$OnlineBackup,
+            c(
+              "Yes" = 1,
+              "No" = 0,
+              "No internet service" = 2
+            ))
+  data$DeviceProtection_1 <-
+    revalue(data$DeviceProtection,
+            c(
+              "Yes" = 1,
+              "No" = 0,
+              "No internet service" = 2
+            ))
+  data$TechSupport_1 <-
+    revalue(data$TechSupport,
+            c(
+              "Yes" = 1,
+              "No" = 0,
+              "No internet service" = 2
+            ))
+  data$StreamingTV_1 <-
+    revalue(data$StreamingTV,
+            c(
+              "Yes" = 1,
+              "No" = 0,
+              "No internet service" = 2
+            ))
+  data$StreamingMovies_1 <-
+    revalue(data$StreamingMovies,
+            c(
+              "Yes" = 1,
+              "No" = 0,
+              "No internet service" = 2
+            ))
+  data$PaperlessBilling_1 <-
+    revalue(data$PaperlessBilling, c("Yes" = 1, "No" = 0))
+  data$Churn_1 <- revalue(data$Churn, c("Yes" = 1, "No" = 0))
+  final_data <-
+    data[-c(1, 2, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 21)]
+  
+  set.seed(1000000)
+  pred <- data.frame(predict(rf,
+                             data = final_data ,
+                             type = "prob"))
+  Prediction <- ifelse(pred[, 2] < 0.26, "Not_Churn", "Churn")
+  probability <- round(pred[, 2], 3)
+  final_tab <- data.frame(data[1], Prediction)
+  return(final_tab)
+}
--- app/Login Module/server.R
+++ app/Login Module/server.R
@@ -129,93 +129,17 @@ server <- function(input, output, session) {
         Sys.sleep(0.1)
       }
     })
-    
-    #data = read.csv("Telco-Customer-Churn- original data - Copy.csv",header = T)
-    data$Gender_1 <- ifelse(data$gender == "Male", 1, 0)
-    
-    data$partner_1 <- ifelse(data$Partner == "Yes", 1, 0)
-    
-    data$Dependents_1 <- ifelse (data$Dependents == "Yes", 1, 0)
-    
-    data$PhoneService_1 <- ifelse(data$PhoneService == "Yes", 1, 0)
-    
-    data$MultipleLines_1 <-
-    
-      revalue(data$MultipleLines,
-              c(
-                "Yes" = 1,
-                "No" = 0,
-                "No phone service" = 2
-              ))
-    data$InternetService_1 <- revalue(data$InternetService, c(
-        "DSL" = 1,
-        "Fiber optic" = 2,
-        "No" = 0
-      ))
-    data$OnlineSecurity_1 <-
-      revalue(data$OnlineSecurity,
-              c(
-                "Yes" = 1,
-                "No" = 0,
-                "No internet service" = 2
-              ))
-    data$OnlineBackup_1 <-
-      revalue(data$OnlineBackup,
-              c(
-                "Yes" = 1,
-                "No" = 0,
-                "No internet service" = 2
-              ))
-    data$DeviceProtection_1 <-
-      revalue(data$DeviceProtection,
-              c(
-                "Yes" = 1,
-                "No" = 0,
-                "No internet service" = 2
-              ))
-    data$TechSupport_1 <-
-      revalue(data$TechSupport,
-              c(
-                "Yes" = 1,
-                "No" = 0,
-                "No internet service" = 2
-              ))
-    data$StreamingTV_1 <-
-      revalue(data$StreamingTV,
-              c(
-                "Yes" = 1,
-                "No" = 0,
-                "No internet service" = 2
-              ))
-    data$StreamingMovies_1 <-
-      revalue(data$StreamingMovies,
-              c(
-                "Yes" = 1,
-                "No" = 0,
-                "No internet service" = 2
-              ))
-    data$PaperlessBilling_1 <-
-      revalue(data$PaperlessBilling, c("Yes" = 1, "No" = 0))
-    data$Churn_1 <- revalue(data$Churn, c("Yes" = 1, "No" = 0))
-    final_data <-
-      data[-c(1, 2, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 21)]
-    
-    set.seed(1000000)
-    pred <- data.frame(predict(rf,
-                               data = final_data ,
-                               type = "prob"))
-    Prediction <- ifelse(pred[, 2] < 0.26, "Not_Churn", "Churn")
-    probability <- round(pred[, 2], 3)
-    final_tab <- data.frame(data[1], Prediction)
-    final_tab
+    print("Sending data to process")
+    final_tab <- process_data(data=data)
     
   })
   
   
-  
+  # Reset Data m,
   observeEvent(input$reset, {
     reset(id = "file")
     rv$data_set <- NULL
+ 
   })
   
   output$fileupload <- renderUI({
